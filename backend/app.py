531f8b59286013551f01ed6fae1e5bec1ddeca65
abolcerek <aaron@bolcerek.com>
2026-01-01 14:08:27 -0500

    added

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+ 
+@app.route('/')
+
+def index():
+    return render_template('index.html')
+
+
+@app.route('/router', methods=['POST'])
+def receive_grid_state():
+    if request.is_json:
+        data = request.get_json()
+
+        return jsonify({"message": "Data received successfully", "the data": data}), 200
+    
+    else:
+        return jsonify({"message": "error with the request"})
